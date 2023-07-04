b1410378619d2fc385ef2ff4539a5bcb2cd8a21d
ortniel <adoumasseo@gmail.com>
2023-07-04 20:23:40 +0100

    prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print alphabet
+ * Return: zero as succes
+ */
+int main(void)
+{
+	int i;
+
+	for (n = 97; n <= 122; n++)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
