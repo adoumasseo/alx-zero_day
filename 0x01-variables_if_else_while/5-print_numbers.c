5b7af2eeb44d7d2e2c627c41842fe24c43c22da8
ortniel <adoumasseo@gmail.com>
2023-08-15 22:23:15 +0100

    Adding 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: It's about print all digit of base 10
+ * Return: as Succes main return 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
