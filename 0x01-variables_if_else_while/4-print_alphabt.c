50b5eb60938709e46a7f4dc03fae7fed5e011780
ortniel <adoumasseo@gmail.com>
2023-08-15 21:54:15 +0100

    Adding 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: it's about print alphabet in lowercase without e & q
+ * return: As succes main return 0
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		if (c != 101 && c != 112)
+			putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
