421804ad9e379230448723ebe03211fcd42bbf1c
ortniel <adoumasseo@gmail.com>
2023-08-15 23:37:23 +0100

    Adding 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: it's about printing single digit with common
+ * Return: 0 as succes
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		putchar(i);
+		if (i <= 56)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
