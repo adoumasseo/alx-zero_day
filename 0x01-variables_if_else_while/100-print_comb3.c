6706f7529cd026a1228600777f053cb0663b9132
ortniel <adoumasseo@gmail.com>
2023-08-17 17:33:56 +0100

    Adding 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: It's about print all combinasons of two singles digits
+ * Return: 0 as succes
+ */
+
+int main(void)
+{
+	int a = 48;
+	int b = 49;
+
+	for (; a <= 56; a++)
+	{
+		for (; b <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+		b = 49;
+		
+	}
+	putchar(10);
+	return (0);
+}
