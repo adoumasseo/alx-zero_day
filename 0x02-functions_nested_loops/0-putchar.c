9069f051b4d204c3f34c424b3aba719e93f414ab
ortniel <adoumasseo@gmail.com>
2023-08-18 20:12:50 +0100

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: It's about printing _putchar followed by a new line
+ * Return: 0 as succes
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/READE.md
@@ -0,0 +1,1 @@
+This repo contains my work about function_nested_loops project in alx programms
