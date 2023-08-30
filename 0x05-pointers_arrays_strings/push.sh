f0f83d5b4742df0604b15cbf46fe3a105a29e778
adoumasseo <adoumasseo@gmail.com>
2023-08-30 16:51:53 +0100

    adsfgh

--- 0x05-pointers_arrays_strings/push.sh
+++ 0x05-pointers_arrays_strings/push.sh
@@ -7,4 +7,4 @@ read -p "Entrez le message de commit : " commit_message
 # Exécute les commandes git en utilisant les variables d'environnement
 git add .
 git commit -m "$commit_message"
-git push https://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@github.com/adoumasseo/alx-low_level_programming.git
+git push
